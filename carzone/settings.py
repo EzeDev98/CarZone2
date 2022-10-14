9358d272c9c27e5755ffa1cff49baee5f26a1e9b
Eze Moses <ezemosesjohn21997@gmail.com>
2022-10-13 23:44:34 -0500

    configured database and whitenoise  middleware

--- carzone/settings.py
+++ carzone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ml3ot=@3bo8+&mpe%=ksbb#3zb8(&bs8gk2f*@z=@c-v(b^@d!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -66,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'carzone.urls'
@@ -92,17 +94,17 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'carzone_db',
-        'USER': 'postgres',
-        'PASSWORD': 'ezepg9898@',
-        'HOST': 'localhost',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'carzone_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ezepg9898@',
+#         'HOST': 'localhost',
+#     }
+# }
 
-
+DATABASES  = {'default': dj_database_url.config(default='postgres://postgres:ezepg9898@@localhost/carzone_db')}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -163,6 +165,10 @@ EMAIL_HOST_USER = 'ezemosesjohn21997@gmail.com'
 EMAIL_HOST_PASSWORD = 'ujyhzrlflghiagrd'
 EMAIL_USE_TLS = True
 
+
+# whitenoise settings
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
